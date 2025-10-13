ae91ec20dc2a5c1911361cf68533b07fa0f0f95d
Dushime Christian <dushimec515@gmail.com>
2025-10-13 18:32:10 +0000

    use this image on SUV image
    https://www.google.com/imgres?q=suv%20cars&

--- next.config.ts
+++ next.config.ts
@@ -23,11 +23,17 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.topgear.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
