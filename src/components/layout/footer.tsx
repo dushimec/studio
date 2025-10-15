56ec919bf0a9dbedd1a36941eaf021ab355c057d
Dushime Christian <dushimec515@gmail.com>
2025-10-15 08:54:38 +0000

    and apply it to footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="border-t border-border/40">
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+        <div className="flex flex-col items-center gap-4 md:flex-row md:gap-2">
           <Logo />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
             Built by you. Powered by Firebase and Google AI.
