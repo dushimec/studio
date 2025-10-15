7192c5a2991701240dafc632ff37adf5c08aad7c
Dushime Christian <dushimec515@gmail.com>
2025-10-15 08:48:51 +0000

    make it litle cycled

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -16,7 +16,7 @@ export function Logo({ className }: { className?: string }) {
         width={100} 
         height={40}
         priority
-        className="h-10 w-auto"
+        className="h-10 w-auto rounded-lg"
       />
     </Link>
   );
