e7dfb957121473cf0528be2e66694cc20109315d
dushimec515 <dushimec515@gmail.com>
2025-10-24 12:47:56 +0000

    changes

--- next.config.ts
+++ next.config.ts
@@ -78,6 +78,19 @@ const nextConfig: NextConfig = {
       'https://9003-firebase-studio-1761126629370.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev',
     ],
   },
+  env: {
+    FIREBASE_TYPE: process.env.FIREBASE_TYPE,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_PRIVATE_KEY_ID: process.env.FIREBASE_PRIVATE_KEY_ID,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    FIREBASE_CLIENT_ID: process.env.FIREBASE_CLIENT_ID,
+    FIREBASE_AUTH_URI: process.env.FIREBASE_AUTH_URI,
+    FIREBASE_TOKEN_URI: process.env.FIREBASE_TOKEN_URI,
+    FIREBASE_AUTH_PROVIDER_X509_CERT_URL: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    FIREBASE_CLIENT_X509_CERT_URL: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    FIREBASE_UNIVERSE_DOMAIN: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  },
 };
 
 export default nextConfig;
