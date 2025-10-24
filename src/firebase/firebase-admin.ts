24c9a8f2060e9b2adcae4becbbb5bb65b624ab1c
dushimec515 <dushimec515@gmail.com>
2025-10-24 12:22:59 +0000

    updates

--- src/firebase/firebase-admin.ts
+++ src/firebase/firebase-admin.ts
@@ -1,10 +1,9 @@
 
 import * as admin from 'firebase-admin';
-import serviceAccount from '../../serviceAccountKey.json';
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.applicationDefault(),
   });
 }
 
